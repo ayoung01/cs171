d5f81b2d43b453e415b2cf66aa68298bad342b32
Albert Young <ayoung01@college.harvard.edu>
2013-04-24 22:01:46 -0400

    added by_use.json

--- project3/data/Processed CSVs/JSONConverter.py
+++ project3/data/Processed CSVs/JSONConverter.py
@@ -22,40 +22,24 @@ class JSONConverter(dict):
             return value
 
 def country():
-    a = JSONConverter()
-
-    a[1][2][3] = 4
-    a[1][3][3] = 5
-    a[1][2]['test'] = 6
-
-
     fcsv = open('country.csv', 'rb')
     fjson = open('country.json', 'wb')
     
-    # holds the 
-    arr = JSONConverter()
+    result = JSONConverter()
     headers = []
     
     line = fcsv.readline()
     lines = line.split('\r\n')
-    # print lines
     headers = lines[0].split(',')
-    # print headers
     
     lines.pop(0)
 
     for line in iter(lambda: fcsv.readline(), ""):
-    # for line in lines:
-        # print line
         row = line.split(',')
         year = row[0].strip()
-        if (not year):
-            print "AHA!: " + str(x)
-            print line
         code = row[2].strip()
         name = row[3].strip()
 
-        temp_dict = {}
         for x in range (4, len(headers)):
             # for each IJAN, ..., IYR, EJAN, ..., EYR
             header = headers[x].strip()
@@ -64,20 +48,61 @@ def country():
 
             # rest of header is the month (may contain 'YR')
             month = header[1:]
-            # print "trade type: " + trade_type
-            # print "month: " + month
 
             # {year: {month: {code: {imports: #, exports: #, balance:#}}}}
             if code != "none":
-                arr[year][month][code][trade_type] = float(row[x])
+                result[year][month][code][trade_type] = float(row[x])
             else:
-                arr[year][month][name][trade_type] = float(row[x])
+                result[year][month][name][trade_type] = float(row[x])
 
-        # arr[row[0]] = temp_dict
-        
-    out = json.dumps(arr)
+    out = json.dumps(result)
     fjson.write(out)
     fcsv.close()
     fjson.close()
 
-country()
+def by_use():
+    fcsv = open('by_use.csv', 'rb')
+    fjson = open('by_use.json', 'wb')
+    
+    result = JSONConverter()
+    headers = []
+    year = ""
+    
+    line = fcsv.readline()
+    lines = line.split('\r\n')
+    headers = lines[0].split(',')
+    
+    lines.pop(0)
+
+    for line in iter(lambda: fcsv.readline(), ""):
+        row = line.split(',')
+        for x in range(1, len(headers)):
+            # for each IJAN, ..., IYR, EJAN, ..., EYR
+            header = headers[x].strip()
+            # first letter of the header (I, E, or B) reveals import, export, or balance
+            trade_type = header[:1]
+            # rest of header is the use
+            use = header[1:]
+
+            # if we encounter a row that is a year, set current year
+            if (row[0][:1] == "1") or (row[0][:1] == "2"):
+                year = row[0]
+                try:
+                    result[year]["total_year"][trade_type][use] = float(row[x])
+                except:
+                    result[year]["total_year"][trade_type][use] = "none"
+            # else we are record the month's data
+            else:
+                month = row[0]
+                # {year: {month: {I: {total: #, foods: #, ...} E: {...}}}}
+                try:
+                    result[year][month][trade_type][use] = float(row[x])
+                except:
+                    result[year][month][trade_type][use] = "none"
+    out = json.dumps(result)
+    fjson.write(out)
+    fcsv.close()
+    fjson.close()
+
+# country()
+by_use()
