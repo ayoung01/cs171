9ad02b7c64e45b95370c04db324eda8b3f1938dc
Albert Young <ayoung01@college.harvard.edu>
2013-05-01 00:18:40 -0400

    fixed buttons

--- project3/js/bootstrap.js
+++ project3/js/bootstrap.js
@@ -223,16 +223,16 @@
 
   var old = $.fn.button
 
-  $.fn.button = function (option) {
-    return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('button')
-        , options = typeof option == 'object' && option
-      if (!data) $this.data('button', (data = new Button(this, options)))
-      if (option == 'toggle') data.toggle()
-      else if (option) data.setState(option)
-    })
-  }
+  // $.fn.button = function (option) {
+  //   return this.each(function () {
+  //     var $this = $(this)
+  //       , data = $this.data('button')
+  //       , options = typeof option == 'object' && option
+  //     if (!data) $this.data('button', (data = new Button(this, options)))
+  //     if (option == 'toggle') data.toggle()
+  //     else if (option) data.setState(option)
+  //   })
+  // }
 
   $.fn.button.defaults = {
     loadingText: 'loading...'
